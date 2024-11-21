a38bdc8e3d50ef26b625a1860801c278e4fb365e
Tatyana Venanzi <tatyanavenanzi@gmail.com>
2024-11-21 10:59:32 -0800

    completed function for tests in mystery 2

--- src/mystery.js
+++ src/mystery.js
@@ -1,1 +1,17 @@
-export const mystery2 = (num) => {};
+// Non-refactored
+export const mystery2 = (num) => {
+  if (num % 400 === 0) {
+    return true;
+  } else if (num % 100 === 0) {
+    return false;
+  } else if (num % 4 === 0) {
+    return true;
+  } else {
+    return false;
+  }
+};
+
+// Refactored
+export const mystery2 = num => {
+  return num % 400 === 0 || (num % 4 === 0 && num % 100 !== 0);
+};
